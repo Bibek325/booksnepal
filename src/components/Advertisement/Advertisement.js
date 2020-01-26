7ecd961d8b784baf31d5b52cca8b4d2cd63b8acf
sachin.basnet <sachin.basnet@hotmail.com>
2020-01-26 10:57:47 +0545

    updated advertisement

--- src/components/Advertisement/Advertisement.js
+++ src/components/Advertisement/Advertisement.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import css from './Advertisement.css'
 import ad from '../../assets/ad.jpg'
-import ad0 from '../../assets/ad1.jpg'
+import ad0 from '../../assets/cloud.jpg'
 const Advertisement = ()=>{
 	return(
 		<div className={css.Advertisement}>
 		<a href='http://nccs.edu.np/college/' target="_blank"><img className={css.ad1}  src={ad}/></a>
-		<img className={css.ad0} src={ad0}/>
+		<a href='https://www.cloudfactory.com.np/' target="_blank"><img className={css.ad0} src={ad0}/></a>
 		</div>
 		)
 }
