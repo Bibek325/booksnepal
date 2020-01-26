cc311b83bfc616d999d874dd9e8d2d5b44fc2e07
Bibek lama <bibeklama67@gmail.com>
2020-01-26 13:28:08 +0545

    Cart Done

--- src/containers/Cart/Cart.js
+++ src/containers/Cart/Cart.js
@@ -1,15 +1,26 @@
 import React,{Component} from 'react'
 import css from './Cart.css'
 import Item from './Item/Item'
+import { connect } from 'react-redux'
 class Cart extends Component{
+	CheckoutHandler=()=>{
+		if(!this.props.isAuth){
+			this.props.history.replace('/login')
+		}
+	}
 	render(){
 		return(
 			<div classname={css.Cart}>
 				<div className={css.title}>Cart</div>
 				<div><Item/></div>
-				<div className={css.Checkout}>Checkout</div>
+				<div className={css.Checkout} onClick={this.CheckoutHandler}>Checkout</div>
 			</div>
 			)
 	}
 }
-export default Cart
+const mapStateToProps=(state)=>{
+	return{
+		isAuth:state.isAuth
+	}
+}
+export default connect(mapStateToProps)(Cart)
