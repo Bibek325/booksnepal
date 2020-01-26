6df7ebd5d3f747cd29942924f044945625032658
lokeshbhusal <bhusallokesh19@gmail.com>
2020-01-26 12:36:51 +0545

    change login form

--- src/containers/Register/Login/Login.js
+++ src/containers/Register/Login/Login.js
@@ -15,7 +15,7 @@ class LoginBar extends Component{
 				elementType:'input',
 				elementConfig:{
 					type:'email',
-					placeholder:'email-id',
+					placeholder:'Email-id',
 					
 				},
 				value:'',
